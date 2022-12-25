946f3e33fafcadfddb685a656f4d700557e02eee
Manasa1013 <66269691+Manasa1013@users.noreply.github.com>
2022-12-25 20:09:51 +0530

    add quiz sample data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React, { useEffect,useState } from 'react';
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { questions } from "./data/getQuiz"
 import './App.css';
 
 function App() {
-  
+  const [quizCategory, setQuizCategory] = useState("")
+  useEffect(() => {
+    setQuizCategory(questions.category)
+  },[])
   return (
     <div className="App">
       React typescript quiz app
+      <h3>{quizCategory}</h3>
     </div>
   );
 }
