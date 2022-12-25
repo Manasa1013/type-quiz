b8c9b2f1c9f4ad33ab0c99533ba2f32c1603a9c4
Manasa1013 <66269691+Manasa1013@users.noreply.github.com>
2022-12-25 19:50:52 +0530

    add tailwind and modify initial setup

---
+++ src/data/getQuiz.ts
@@ -0,0 +1,31 @@
+export const questions = {
+    category: "music",
+    questions: [
+        {
+            question: "Who won the prestigious Grammy award this year?",
+            points : "10",
+            options: [
+                {
+                    key: "a",
+                    text: "Justin bieber",
+                    isRight : false
+                },
+                {
+                    key: "b",
+                    text: "Taylor Swift",
+                    isRight : false
+                },
+                {
+                    key: "a",
+                    text: "Ariana Grande",
+                    isRight : true
+                },
+                {
+                    key: "a",
+                    text: "Shawn Mendes",
+                    isRight : false
+                }
+            ]
+        }
+    ]
+}
